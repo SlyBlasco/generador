18f6b86e2a421f2741345680fa50eebf7aba5100
luism <luism@LuxhPC>
2023-10-08 13:50:32 -0700

    Inicio de proyecto y creacion de paquete entidades con clase data.java (con variables)

---
+++ src/mx/itson/generador/entidades/data.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.generador.entidades;
+
+
+/**
+ *
+ * @author luism
+ */
+public class data {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private int fechaNacimiento;
+    private char sexo;
+    private String entidad;
+    
+    
+}
